ed03164fd5b1c392154489b506c26b2822f9a6f5
Gavin Gosling <84335744+gavingosling@users.noreply.github.com>
2021-06-03 17:46:18 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,9 @@ set -eax
 interrogate --version
 
 if interrogate --fail-under $2 --generate-badge $3 $1 | grep -q 'PASSED'; then
-    interrogate --fail-under $2 --generate-badge $3 $1
+    interrogate --fail-under $2 --generate-badge $3 $1 -vv
     exit 0
 else 
-    interrogate --fail-under $2 --generate-badge $3 $1
+    interrogate --fail-under $2 --generate-badge $3 $1 -vv
     exit 1
 fi

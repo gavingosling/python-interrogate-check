3aec8502f3a0c7ad37e9c0c4f01d8d97304ceb56
Gavin Gosling <84335744+gavingosling@users.noreply.github.com>
2021-06-03 21:24:17 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eax
-
 interrogate --version
 interrogate --fail-under $2 --generate-badge $3 $1 -vv > badges/table.txt
-sed '$d' badges/table.txt
+set +eax
+sed -i '$d' badges/table.txt
 exit 0
